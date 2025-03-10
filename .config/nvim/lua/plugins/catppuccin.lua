a2075fefbe52fcbcb8fcdb8b076588294dee2796
yasasvi <yasasvigumma@gmail.com>
2025-03-10 11:43:07 +0530

    Trying different themes
    ...

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -2,17 +2,17 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  config = function ()
+  config = function()
     require("catppuccin").setup({
       transparent_background = true,
       styles = {
-        loops = {"italic"},
-        functions = {"italic"},
-        keywords = {"italic"},
-        types = {"italic"},
-        booleans = {"italic"},
+        loops = { "italic" },
+        functions = { "italic" },
+        keywords = { "italic" },
+        types = { "italic" },
+        booleans = { "italic" },
       },
     })
-    vim.cmd.colorscheme "catppuccin-macchiato"
+    vim.cmd("colorscheme catppuccin-macchiato")
   end
 }
