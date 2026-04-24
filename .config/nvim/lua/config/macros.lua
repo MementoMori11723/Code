93cfc751d24b8c8447045758ab8cf0a886101ff6
Yasasvi Gumma <yasasvigumma@gmail.com>
2026-04-24 11:44:03 +0530

    Added theme
    ...

--- .config/nvim/lua/config/macros.lua
+++ .config/nvim/lua/config/macros.lua
@@ -3,19 +3,19 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-r>", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<C-l>", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<C-p>", function()
-	builtin.find_files({
-		hidden = true,
-	})
+  builtin.find_files({
+    hidden = true,
+  })
 end, { desc = "Telescope find files" })
 
 -- Auto save
 vim.api.nvim_create_augroup("LspFormatOnSave", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
-	group = "LspFormatOnSave",
-	pattern = "*",
-	callback = function()
-		vim.lsp.buf.format({ async = false })
-	end,
+  group = "LspFormatOnSave",
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
 })
 
 -- Code info
@@ -26,24 +26,24 @@ vim.keymap.set("n", "Ca", vim.lsp.buf.code_action, {})
 
 -- Dismiss notifications
 vim.keymap.set("n", "<C-d>", "<cmd>NoiceDismiss<CR>", {
-	noremap = true,
-	silent = true,
+  noremap = true,
+  silent = true,
 })
 
 -- Toggle file tree
 vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {
-	noremap = true,
-	silent = true,
+  noremap = true,
+  silent = true,
 })
 
 -- Close buffers
 vim.keymap.set("n", "<C-c>", ":BufferLineCloseOther<CR>", {
-	noremap = true,
-	silent = true,
+  noremap = true,
+  silent = true,
 })
 
 -- Toggle terminal
 vim.keymap.set("n", "<C-t>", '<cmd>lua require("toggleterm").toggle()<CR>', {
-	noremap = true,
-	silent = true,
+  noremap = true,
+  silent = true,
 })
